1669468bb5bc2111db15f4de501cb717a9f93622
dalhat <bekind2023.1@gmail.com>
2025-12-03 16:33:52 +0530

    first commit of sample project

---
+++ src/test/java/Guide/Guide/Pract.java
@@ -0,0 +1,17 @@
+package Guide.Guide;
+
+public class Pract {
+
+	public static void main(String[] args) {
+	
+		//Count the number of characters in string
+		String k ="TotalNumberofpair";
+		
+		for(int i=0;i<k.length();i++) 
+		{
+			
+		}
+		System.out.println(k.length());
+	}
+
+}
